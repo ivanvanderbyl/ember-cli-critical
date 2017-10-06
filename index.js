c3106049b3100d3f02ab0f7346ba695f721b2625
Ivan Vanderbyl <ivanvanderbyl@gmail.com>
2017-10-06 19:23:16 +0200

    Process build through critical

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 /* jshint node: true */
-'use strict';
+'use strict'
 
+var critical = require('critical')
 module.exports = {
-  name: 'ember-cli-critical-css'
-};
+  name: 'ember-cli-critical-css',
+
+  async postBuild(result) {
+    await critical
+      .generate({
+        inline: true,
+        minify: true,
+        base: result.directory,
+        src: 'index.html',
+        dest: 'index.html',
+        width: 1300,
+        height: 900,
+      })
+  },
+}
