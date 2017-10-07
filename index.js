a2dcd08e0b0aa064b3a9b42851af792e55d54f14
Ivan Vanderbyl <ivanvanderbyl@gmail.com>
2017-10-07 10:13:29 +0200

    Configured to allow settings

--- index.js
+++ index.js
@@ -1,20 +1,57 @@
 /* jshint node: true */
 'use strict'
 
-var critical = require('critical')
-module.exports = {
-  name: 'ember-cli-critical-css',
+const critical = require('critical')
+const fs = require('fs')
+const path = require('path')
+const defaults = require('lodash.defaultsdeep')
 
-  async postBuild(result) {
-    await critical
-      .generate({
+module.exports = {
+  name: 'ember-cli-critical',
+
+  included(app) {
+    let defaultOptions = {
+      enabled: app.env === 'production',
+
+      critical: {
         inline: true,
-        minify: true,
-        base: result.directory,
+        minify: false,
+        extract: true,
         src: 'index.html',
         dest: 'index.html',
         width: 1300,
         height: 900,
-      })
+      },
+    }
+    this._options = defaults(this._getAddonOptions()[this.name] || {}, defaultOptions)
+  },
+
+  async postBuild(result) {
+    let addonOptions = this._options
+
+    if (addonOptions.enabled) {
+      let cssFiles = fs
+        .readdirSync(path.join(result.directory, 'assets'))
+        .filter(path => path.endsWith('.css'))
+        .map(file => path.posix.join(result.directory, 'assets', file))
+
+      await critical.generate(
+        defaults(addonOptions.critical, {
+          inline: true,
+          minify: false,
+          extract: true,
+          base: result.directory,
+          src: 'index.html',
+          dest: 'index.html',
+          css: cssFiles,
+          width: 1300,
+          height: 900,
+        }),
+      )
+    }
+  },
+
+  _getAddonOptions: function() {
+    return (this.parent && this.parent.options) || (this.app && this.app.options) || {}
   },
 }
